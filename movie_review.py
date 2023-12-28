7bd9d30561515c45e3a36fd5d5c77caf955a26fd
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2023-12-28 16:56:39 -0600

    docker

--- movie_review.py
+++ movie_review.py
@@ -4,6 +4,7 @@ from tensorflow.python.client import device_lib
 def get_available_devices():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos]
+print("hello world")
 print(get_available_devices()) 
 
 # import pandas as pd
