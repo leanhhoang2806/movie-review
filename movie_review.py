d3922a2032d824b7a8a45406eb69e80aa3743025
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-04 14:17:04 -0600

    Jan 4th

--- movie_review.py
+++ movie_review.py
@@ -10,6 +10,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from transformers import BertTokenizer
 from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import AdamW
+from tensorflow.keras.layers import Input, Dense, LayerNormalization
+from tensorflow.keras.models import Model
 
 # Load the IMDb dataset
 csv_file_path = './IMDB Dataset.csv'
@@ -70,16 +72,83 @@ X = np.array(df['review_token'].tolist())
 Y = np.array(df['movie_names_token'].tolist())
 output_size = Y.shape[1]
 
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.query_dense = Dense(d_model)
+        self.key_dense = Dense(d_model)
+        self.value_dense = Dense(d_model)
+
+        self.dense = Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        query, key, value = inputs['query'], inputs['key'], inputs['value']
+        batch_size = tf.shape(query)[0]
+
+        # Linear layers
+        query = self.query_dense(query)
+        key = self.key_dense(key)
+        value = self.value_dense(value)
+
+        # Split heads
+        query = self.split_heads(query, batch_size)
+        key = self.split_heads(key, batch_size)
+        value = self.split_heads(value, batch_size)
+
+        # Scaled dot-product attention
+        scaled_attention = scaled_dot_product_attention(query, key, value)
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+        # Concatenate heads
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
+
+        # Final linear layer
+        outputs = self.dense(concat_attention)
+
+        return outputs
+
+# Scaled Dot-Product Attention Layer
+def scaled_dot_product_attention(query, key, value):
+    matmul_qk = tf.matmul(query, key, transpose_b=True)
+    dk = tf.cast(tf.shape(key)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+    output = tf.matmul(attention_weights, value)
+    return output
+
+def build_model(input_shape, output_size):
+    inputs = Input(shape=(input_shape,))
+    x = Dense(64, activation='relu')(inputs)
+    
+    # Add Multi-Head Attention
+    attention = MultiHeadAttention(d_model=64, num_heads=4)({
+        'query': x,
+        'key': x,
+        'value': x
+    })
+    x = LayerNormalization(epsilon=1e-6)(x + attention)
+    
+    x = Dense(output_size, activation='softmax')(x)
+
+    model = Model(inputs=inputs, outputs=x)
+    return model
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # Build the model
-model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(X.shape[1],)),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(output_size, activation='softmax')  # Adjust based on your output size
-])
+model = build_model(X.shape[1], output_size)
 
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
