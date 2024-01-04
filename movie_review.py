490adbcb11720274bd252055aadebba73dd67bb1
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-04 11:37:20 -0600

    Jan 4th

--- movie_review.py
+++ movie_review.py
@@ -60,7 +60,7 @@ max_movie_length = max(extracted_df['movie_names_token'].apply(len))
 # Apply padding to the DataFrame
 extracted_df['review_token'] = extracted_df['review_token'].apply(lambda x: pad_sequences([x], maxlen=max_review_length, padding='post', truncating='post')[0])
 extracted_df['movie_names_token'] = extracted_df['movie_names_token'].apply(lambda x: pad_sequences([x], maxlen=max_movie_length, padding='post', truncating='post')[0])
-df = extracted_df
+df = extracted_df[['review_token', 'movie_names_token']]
 
 print(df.head())
 
