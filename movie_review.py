8aa377b8c85ccbc5b90e2120bea31a50b3190734
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-05 16:44:22 -0600

    Jan 5th reset

--- movie_review.py
+++ movie_review.py
@@ -136,12 +136,8 @@ def scaled_dot_product_attention(query, key, value):
 def build_complex_model(input_shape, output_size, num_layers, layer_size, dropout_rate, num_heads):
     inputs = Input(shape=(input_shape,))
     x = Dense(layer_size // 2, activation='relu')(inputs)  # Reduce layer size
-        
-    # Use BERT model as embedding layer
-    bert_model = TFBertModel.from_pretrained('bert-base-uncased')
-    bert_output = bert_model(inputs)[0]
+
     
-    x = Dense(layer_size // 2, activation='relu')(bert_output[:, 0, :]) 
     attention = MultiHeadAttention(d_model=layer_size // 2, num_heads=num_heads)({
         'query': tf.expand_dims(x, 1),
         'key': tf.expand_dims(x, 1),
