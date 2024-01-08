42fe06038c5caa604051c2901d72cb3de308fe14
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-08 13:22:25 -0600

    Jan 5th refactored

--- movie_review.py
+++ movie_review.py
@@ -44,10 +44,10 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
     param_grid = {
-        'num_layers': range(1, 3),
-        'layer_size': [256 * i for i in range(1, 5)],
-        'dropout_rate': [0.2 * i for i in range(1, 5)],
-        'num_heads': [i * 2  for i in range(1, 3)],
+        'num_layers': [1],
+        'layer_size': [256],
+        'dropout_rate': [0.2],
+        'num_heads': [4],
     }
 
     best_accuracy, best_params, best_model = grid_search(param_grid, X_train, y_train, X_test, y_test, X.shape[1], output_size)
@@ -56,7 +56,8 @@ def main():
     print(f"Saving to {directory_path}")
     print(f'Best Model Accuracy: {best_accuracy} with best params: {best_params}')
 
-    predicted_df = predict_movie_name(extracted_df, best_model)
+    predicted_df = predict_movie_name(extracted_df, best_model, tokenizer)
+    print(predicted_df.head())
     # Create the directory if it doesn't exist
     os.makedirs(directory_path, exist_ok=True)
     print(predicted_df.head())
