ddffb647de2e101e3c764308f2169a6a824693da
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-08 09:31:02 -0600

    Jan 5th refactored

--- training_strategy/distributed_training.py
+++ training_strategy/distributed_training.py
@@ -6,6 +6,7 @@ from models.model_builder import build_complex_model
 def grid_search(param_grid, X_train, y_train, X_test, y_test, input_shape, output_size):
     best_accuracy = 0
     best_params = None
+    best_model = None
 
     strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
 
@@ -23,5 +24,8 @@ def grid_search(param_grid, X_train, y_train, X_test, y_test, input_shape, outpu
         if accuracy > best_accuracy:
             best_accuracy = max(accuracy, best_accuracy)
             best_params = params
+            best_model = model
+    
+    model.save('./movie_review_model.h5')
 
     return best_accuracy, best_params
