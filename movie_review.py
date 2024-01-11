14647817f93659060b741282ce23792e25b9a629
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-10 19:20:08 -0600

    Jan 10th

--- movie_review.py
+++ movie_review.py
@@ -79,12 +79,14 @@ def main():
 
     # Build a simple neural network using TensorFlow's Keras API
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(10, activation='relu', input_shape=(max_review_length,)),
-        tf.keras.layers.Dense(max_movie_length, activation='softmax'),
-        tf.keras.layers.Reshape((max_movie_length, 1))
+        tf.keras.layers.Dense(10, activation='relu', input_shape=(max_review_length, max_movie_length)),
+        tf.keras.layers.Reshape((max_movie_length,)),
+        tf.keras.layers.Dense(max_movie_length, activation='softmax')
     ])
 
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
 
 
 
