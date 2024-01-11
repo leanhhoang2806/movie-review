bdd85ace65d184bb3c5c35f6ba8faf421696965b
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-11 10:21:08 -0600

    Jan 10th

--- movie_review.py
+++ movie_review.py
@@ -71,6 +71,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer
 from sklearn.metrics import mean_squared_error
+import string
+from itertools import chain, product
 
 def load_imdb_dataset(file_path):
     imdb_df = pd.read_csv(file_path)
@@ -108,6 +110,11 @@ def convert_tokens_to_words(tokenizer, tokens):
     words = tokenizer.convert_tokens_to_string(tokens)
     return " ".join(words)
 
+def get_bert_token(word):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    token_ids = tokenizer.encode(word, add_special_tokens=False, padding=True, truncation=True, max_length=512)
+    return token_ids
+
 def main():
     tf.keras.backend.clear_session()
 
@@ -120,11 +127,32 @@ def main():
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
     for review in imdb_df['review']:
-        data = preprocess_review_data(review, tokenizer, movie_name_pattern)
-        if data:
-            extracted_data.append(data)
+    # Extract text between quotation marks using the defined pattern
+        matches = re.findall(movie_name_pattern, review)
+
+        # Filter out names that are not in capitalization format
+        valid_names = [name for name in matches if name.istitle()]
+
+        # Clean up punctuation at the beginning or end of each title
+        cleaned_names = [name.strip(string.punctuation) for name in valid_names]
+
+        # Append the review and cleaned text to the list if the list is not empty
+        if cleaned_names and len(cleaned_names) == 1:
+            corrected_name = cleaned_names[0].strip()
+            if len(corrected_name.split()) == 2:
+                # Get BERT token for movie name
+                movie_name_tokens_nested = [get_bert_token(token) for token in corrected_name.split()]
+                movie_name_tokens_flatten = list(chain(*movie_name_tokens_nested))
+
+                # Get BERT token for review
+                review_tokens = get_bert_token(review)
+                extracted_data.append({'review_token': review_tokens, 'movie_names_token': movie_name_tokens_flatten,
+                                    "review": review, "movie_names": corrected_name})
+
     extracted_df = pd.DataFrame(extracted_data)
     print(extracted_df.head())
+    print(extracted_df['review'].iloc[0])
+    print(extracted_df['movie_names'].iloc[0])
 
     max_review_length = max([ len(i) for i in pd.Series(extracted_df['review_token'])])
     max_movie_length = max([ len(i) for i in pd.Series(extracted_df['movie_names_token'])])
