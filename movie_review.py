10183ca7dccc6b02133f94c5979675495f8a8c6c
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-11 15:47:21 -0600

    Jan 10th

--- movie_review.py
+++ movie_review.py
@@ -41,7 +41,8 @@ def main():
     Y = extracted_df[['movie_names_token']]
     print(f"extracted df shape {extracted_df['review_token'].shape}")
     print(f"extracted df shape {extracted_df['movie_names_token'].shape}")
-    print(f"extracted df review_token data type {type(tf.constant(extracted_df['review_token']))}")
+    print(f"extract df review token data type {type(extracted_df['review_token'])}")
+    print(f"extracted df review_token data type {type(tf.constant(extracted_df['review_token'].values))}")
 
     # Input data
     X = [[1, 2, 1], [2, 1, 2], [3, 5, 1]]
