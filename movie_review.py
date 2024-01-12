8353e565af98de50562b3615463b40580e518a6f
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-11 18:36:42 -0600

    Jan 10th

--- movie_review.py
+++ movie_review.py
@@ -17,6 +17,7 @@ def main():
 
     csv_file_path = './IMDB Dataset.csv'
     imdb_df = load_imdb_dataset(csv_file_path)
+    print(f"target for testing predictionis : {imdb_df.iloc[0]}")
 
     movie_name_pattern = re.compile(r'"([^"]+)"')
 
@@ -29,21 +30,19 @@ def main():
         if data:
             extracted_data.append(data)
 
-    if not extracted_data:
-        print("No valid data found. Exiting.")
-        return
-
     max_review_length = max([len(data['review_token']) for data in extracted_data])
     max_movie_length = max([len(data['movie_names_token']) for data in extracted_data])
     extracted_df = preprocess_df(extracted_data, max_review_length, max_movie_length)
+    
+    test_review_token = extracted_df['review'].iloc[0]
+    expected_result = extracted_df['movie_names'].iloc[0]
+
     # Split the data into features (X) and target (y)
     X = tf.constant(extracted_df['review_token'].tolist())
     Y = tf.constant(extracted_df['movie_names_token'].tolist())
     input_shape = len(extracted_df['review_token'].tolist()[0])
     output_shape = len(extracted_df['movie_names_token'].tolist()[0])
 
-    print(f"X.shape: {len(extracted_df['review_token'].tolist()[0])}")
-    print(f"Y.shape: {len(extracted_df['movie_names_token'].tolist()[0])}")
     # Define the neural network model
     model = keras.Sequential([
         layers.Dense(units=64, activation='relu', input_shape=(input_shape,)),
@@ -51,13 +50,15 @@ def main():
     ])
 
     # Compile the model
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 
     # Train the model
     model.fit(X, Y, epochs=1, verbose=1)
 
     # Make predictions
-    predictions = model.predict(X)
+    predictions = model.predict(test_review_token)
+    decoded_predictions = tokenizer.decode(predictions[0], skip_special_tokens=True)
+    print(f"Given a review {test_review_token},  \n the movie name prediction is {decoded_predictions}, \n the actual movie name is {expected_result} ")
 
 
     
