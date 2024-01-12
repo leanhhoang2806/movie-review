ce12178784999f472ef2f224310011613279ee89
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-01-12 10:38:25 -0600

    Jan 10th

--- movie_review.py
+++ movie_review.py
@@ -1,81 +1,68 @@
-# import re
-# import pandas as pd
-# import numpy as np
-# from sklearn.model_selection import train_test_split
-# from transformers import BertTokenizer
-# import tensorflow as tf
-# from data_loader.load_imdb import load_imdb_dataset
-# from processors.tokenizer import preprocess_review_data, preprocess_df
-# from training_strategy.distributed_training import grid_search
-# import os
-# from tensorflow import keras
-# from tensorflow.keras import layers
-
-
-# def main():
-#     tf.keras.backend.clear_session()
-
-#     csv_file_path = './IMDB Dataset.csv'
-#     imdb_df = load_imdb_dataset(csv_file_path)
-#     print(f"target for testing predictionis : {imdb_df.iloc[0]}")
-
-#     movie_name_pattern = re.compile(r'"([^"]+)"')
-
-#     extracted_data = []
-
-#     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-#     for review in imdb_df['review']:
-#         data = preprocess_review_data(review, tokenizer, movie_name_pattern)
-#         if data:
-#             extracted_data.append(data)
-
-#     max_review_length = max([len(data['review_token']) for data in extracted_data])
-#     max_movie_length = max([len(data['movie_names_token']) for data in extracted_data])
-#     extracted_df = preprocess_df(extracted_data, max_review_length, max_movie_length)
-    
-
-#     # Split the data into features (X) and target (y)
-#     X = tf.constant(extracted_df['review_token'].tolist())
-#     Y = tf.constant(extracted_df['movie_names_token'].tolist())
-#     input_shape = len(extracted_df['review_token'].tolist()[0])
-#     output_shape = len(extracted_df['movie_names_token'].tolist()[0])
-
-#     num_heads = 2
-#     d_model = 4
-
-#     inputs = {'query': X, 'key': X, 'value': X}
-#     attention_layer = MultiHeadAttention(d_model, num_heads)
-#     output = attention_layer(inputs)
-
-#     # Define the neural network model
-#     model = keras.Sequential([
-#         layers.Dense(units=64, activation='relu', input_shape=(input_shape,)),
-#         layers.Dense(units=output_shape)  # Output layer with 2 units for the target values
-#     ])
-
-#     # Compile the model
-#     model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
-
-#     # Train the model
-#     model.fit(X, Y, epochs=1000, verbose=1)
-
-#     predictions = model.predict(X)
-#     decoded_predictions = tokenizer.decode(predictions[0], skip_special_tokens=True)
-#     print(f"the movie name prediction is {decoded_predictions}, \n the actual review is  {extracted_data[0]['review']} ")
-
-
-    
-# if __name__ == "__main__":
-#     main()
-
-
+import re
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer
 import tensorflow as tf
-from tensorflow.keras.layers import Dense
+from data_loader.load_imdb import load_imdb_dataset
+from processors.tokenizer import preprocess_review_data, preprocess_df
+from training_strategy.distributed_training import grid_search
+import os
+from tensorflow import keras
+from tensorflow.keras import layers
 
-# Input data
-X = tf.constant([[1, 2, 1], [2, 1, 2], [3, 5, 1]], dtype=tf.float32)
-Y = tf.constant([[2, 0], [2, 1], [5, 4]], dtype=tf.float32)
+
+def main():
+    tf.keras.backend.clear_session()
+
+    csv_file_path = './IMDB Dataset.csv'
+    imdb_df = load_imdb_dataset(csv_file_path)
+    print(f"target for testing predictionis : {imdb_df.iloc[0]}")
+
+    movie_name_pattern = re.compile(r'"([^"]+)"')
+
+    extracted_data = []
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    for review in imdb_df['review']:
+        data = preprocess_review_data(review, tokenizer, movie_name_pattern)
+        if data:
+            extracted_data.append(data)
+
+    max_review_length = max([len(data['review_token']) for data in extracted_data])
+    max_movie_length = max([len(data['movie_names_token']) for data in extracted_data])
+    extracted_df = preprocess_df(extracted_data, max_review_length, max_movie_length)
+    
+
+    # Split the data into features (X) and target (y)
+    X = tf.constant(extracted_df['review_token'].tolist())
+    Y = tf.constant(extracted_df['movie_names_token'].tolist())
+
+
+    # Instantiate the model
+    model = MyModel(d_model=4, num_heads=2)
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    # Train the model
+    model.fit({'query': X, 'key': X, 'value': X}, Y, epochs=1000, verbose=1)
+
+    # Evaluate the model
+    predictions = model({'query': X, 'key': X, 'value': X})
+    print("Input:")
+    print(X.numpy())
+    print("\nGround Truth:")
+    print(Y.numpy())
+    print("\nPredictions:")
+    print(predictions.numpy())
+
+
+    
+if __name__ == "__main__":
+    main()
+
 
 # Multihead Attention layer
 class MultiHeadAttention(tf.keras.layers.Layer):
@@ -149,22 +136,5 @@ class MyModel(tf.keras.Model):
         x = self.ffn(x)
         return x
 
-# Instantiate the model
-model = MyModel(d_model=4, num_heads=2)
-
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-# Train the model
-model.fit({'query': X, 'key': X, 'value': X}, Y, epochs=1000, verbose=1)
-
-# Evaluate the model
-predictions = model({'query': X, 'key': X, 'value': X})
-print("Input:")
-print(X.numpy())
-print("\nGround Truth:")
-print(Y.numpy())
-print("\nPredictions:")
-print(predictions.numpy())
 
 
